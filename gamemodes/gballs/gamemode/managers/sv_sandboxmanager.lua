ceed6ee9e21e22d27fa4bf8e1ba4054144d3f405
David <davidmoussa@outlook.com>
2019-12-29 20:51:39 +0200

    Added Sandbox Manager

---
+++ gamemodes/gballs/gamemode/managers/sv_sandboxmanager.lua
@@ -0,0 +1,13 @@
+function GB:InitSanbox()
+    local spawn = ents.Create("prop_physics")
+    if not IsValid(spawn) then return end
+    spawn:SetModel("models/gballs/square_4x4.mdl")
+    spawn:SetPos(Vector(0, 0, 0))
+    spawn:Spawn()
+end
+
+hook.Add("OnGameSet", "SetSandbox", function(gameType)
+    if gameType == GB.GameState.SANDBOX then
+        GB:InitSanbox()
+    end
+end)
