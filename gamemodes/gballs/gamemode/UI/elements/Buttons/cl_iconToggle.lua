835bd3da6e2fceb57a1c64c88e20b510052520bb
David <davidmoussa@outlook.com>
2019-12-31 03:12:30 +0200

    Implemented Toggle

---
+++ gamemodes/gballs/gamemode/UI/elements/Buttons/cl_iconToggle.lua
@@ -0,0 +1,88 @@
+ICONTOGGLE = {}
+
+local mats = {
+    EXIT = Material("gballs/icons/exit.png", "smooth")
+}
+
+function ICONTOGGLE:Init()
+    self.rand = math.random(0, 9999)
+    self.panel = vgui.Create("DButton", self)
+    self.panel:SetText("")
+    self.panel.selectedColor = Color(255, 50, 50)
+    self.panel.unSelectedColor = Color(255, 255, 255)
+    self.panel.color = self.panel.unSelectedColor
+    self.panel.pressed = false
+    self.panel.hovered = false
+    self.panel.active = false
+    self:SetAction()
+
+    function self.panel:Think()
+        if self:IsHovered() and not self.hovered then
+            self.hovered = true
+
+            LerpColor(self.color, self.selectedColor, 0.1, function(col)
+                self.color = col
+            end, INTERPOLATION.SmoothStep)
+        elseif not self:IsHovered() and self.hovered then
+            self.hovered = false
+
+            LerpColor(self.color, self.active and self.selectedColor or self.unSelectedColor, 0.1, function(col)
+                self.color = col
+            end, INTERPOLATION.SmoothStep)
+        end
+    end
+end
+
+function ICONTOGGLE:PerformLayout(width, height)
+    self.panel:SetSize(height, height)
+    self.panel:Center()
+end
+
+function ICONTOGGLE:SetColors(selected, unselected)
+    self.panel.selectedColor = selected
+    self.panel.unSelectedColor = unselected
+end
+
+function ICONTOGGLE:SetMat(mat)
+    function self.panel:Paint(width, height)
+        draw.DrawRect(0, 0, width, height, self.color, mat)
+
+        if  (self.hovered or self.pressed) and not self.active then
+            draw.DrawRect(0, 0, width, height, Color(255, 255, 255, 50), mat)
+        end
+    end
+end
+
+function ICONTOGGLE:SetAction(action)
+    self.panel.DoClick = function()
+        if action then
+            action()
+        end
+
+        self.panel.active = not self.panel.active
+
+        LerpColor(self.panel.color, self.panel.active and self.panel.selectedColor or self.panel.unSelectedColor, 0.1, function(col)
+            self.panel.color = col
+        end, INTERPOLATION.SmoothStep)
+    end
+end
+
+function ICONTOGGLE:SetKey(key)
+    hook.Add("CreateMove", "PressControls" .. self.rand, function()
+        if key == 0 then return end
+
+        if input.WasKeyPressed(key) then
+            self.panel.pressed = true
+        end
+
+        if input.WasKeyReleased(key) then
+            self.panel.pressed = false
+
+            if self.panel.DoClick then
+                self.panel:DoClick()
+            end
+        end
+    end)
+end
+
+vgui.Register("gb_icontoggle", ICONTOGGLE)
