7adf8fede8ce429867a910d2bddfba2ffa2dcb50
David <davidmoussa@outlook.com>
2020-01-08 22:57:05 +0200

    Started With Object Placer

---
+++ gamemodes/gballs/gamemode/Sandbox/cl_prop_placer.lua
@@ -0,0 +1,14 @@
+GB.placedProps = {}
+GB.heldProp = nil
+
+function GB:PlaceEntity(ent)
+end
+
+function GB:SnapToGrid(pos)
+end
+
+function GB:ValidatePosition()
+end
+
+function GB:DrawGizmo()
+end
---
+++ gamemodes/gballs/gamemode/Sandbox/cl_sandbox_grid.lua
@@ -0,0 +1,3 @@
+GB.grid = nil
+GB.tileSize = 64
+GB.GridSquareSize = 128
