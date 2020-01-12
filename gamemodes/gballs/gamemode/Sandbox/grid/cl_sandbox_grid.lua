efaa3d32f15d11a81c256107b5303f3f8120c386
David <davidmoussa@outlook.com>
2020-01-12 22:48:25 +0200

    Added Multi Tile Support

--- gamemodes/gballs/gamemode/Sandbox/grid/cl_sandbox_grid.lua
+++ gamemodes/gballs/gamemode/Sandbox/grid/cl_sandbox_grid.lua
@@ -1,17 +1,46 @@
 function GB:CreateTile(sizeX, sizeY)
-    self.tile = ents.CreateClientProp()
-    self.tile:SetModel("models/gballs/gridtile.mdl")
-    self.tile:SetPos(Vector(0, 0, 0))
-    self.tile:Spawn()
+    if self.tiles and self.tiles ~= {} then
+        for k, v in pairs(self.tiles) do
+            if IsValid(v) then
+                v:Remove()
+            end
+        end
+    end
+
+    self.tiles = {}
+
+    for i = 1, sizeY do
+        for j = 1, sizeX do
+            local tile = ents.CreateClientProp()
+            tile:SetModel("models/gballs/gridtile.mdl")
+            tile:Spawn()
+            tile:SetColor(0,255,0, 255)
+            table.insert(self.tiles, tile)
+        end
+    end
+
+    self:SetTilePos(self.tiles, sizeX, sizeY)
 
     hook.Add("Think", "PlaceTile", function()
-        self.tile:SetPos(self.sandboxCamera:GetMouseTrace(5000).HitPos)
-        local tile = self:GetTile(self.tile)
-        self:SetTile(self.tile, tile)
-        self.tile:SetPos(self.tile:GetPos() + Vector(self.tileSize, self.tileSize, 0) / 2)
+        self:SetTilePos(self.tiles, sizeX, sizeY)
     end)
 end
 
+function GB:SetTilePos(tiles, sizeX, sizeY)
+    local index = 1
+
+    for i = 1, sizeY do
+        for j = 1, sizeX do
+            print(index)
+            tiles[index]:SetPos(self.sandboxCamera:GetMouseTrace(5000).HitPos + Vector(self.tileSize * (j - 1), self.tileSize * (i - 1), 0))
+            local tile = self:GetTile(tiles[index])
+            self:SetTile(tiles[index], tile)
+            tiles[index]:SetPos(tiles[index]:GetPos() + Vector(self.tileSize, self.tileSize, 0) / 2)
+            index = index + 1
+        end
+    end
+end
+
 net.Receive("CreateTile", function()
-    GB:CreateTile()
+    GB:CreateTile(5, 5)
 end)
